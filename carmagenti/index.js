736ff9d7c062eff1afd30da6e0482172f0bce236
trlla2 <roc.allue@enti.cat>
2024-01-24 20:12:16 +0100

    a

--- carmagenti/index.js
+++ carmagenti/index.js
@@ -6,9 +6,6 @@ var file = new static.Server('./public');
  
 let http_server = http.createServer(function (request, response) {
     request.addListener('end', function () {
-        //
-        // Serve files!
-        //
         file.serve(request, response);
     }).resume();
 });
@@ -23,6 +20,7 @@ let p2_conn;
 
 
 ws_server.on('connection', function (conn){
+   
     console.log('EVENT: Conexion');
 
     if(p1_conn == undefined){
@@ -31,19 +29,25 @@ ws_server.on('connection', function (conn){
         p1_conn.send('{"player_num":1}');
 
         p1_conn.on('message', function(data){
-            //p2_conn.send(data);
-            console.log(data.toString());
-        })
+            if(p2_conn == undefined){
+                return;
+            }
+
+            p2_conn.send(data.toString());
+        });
     }
     else if(p2_conn == undefined){
         p2_conn = conn;
 
-        p1_conn.send('{"player_num":2}');
+        p2_conn.send('{"player_num":2}');
 
         p2_conn.on('message', function(data){
-            p1_conn.send(data);
-            console.log(data.toString());
-        })
+            if(p1_conn == undefined){
+                return;
+            }
+            p1_conn.send(data.toString());
+        });
+
     }
 
    /* conn.on('message', function(data){
--- carmagenti/public/camagenti.js
+++ carmagenti/public/camagenti.js
@@ -1,4 +1,10 @@
+
+
 let player_num = 0;
+
+let player1;
+let player2;
+
 
 const socket = new WebSocket("ws://10.40.2.186:8080");
 
@@ -13,6 +19,27 @@ socket.addEventListener("message",function(event){
     if(data.player_num != undefined){
         player_num = data.player_num;
         console.log("Jugador numero: ", player_num);
+    }
+    else if (data.x != undefined){
+        if(player_num == 1){
+            player2.x = data.x;
+            player2.y = data.y;
+            player2.rotation = data.r;
+            
+            bullet2.x = data.bx;
+            bullet2.y = data.by;
+            bullet2.rotation = data.br;
+        }
+        if(player_num == 2){
+            player1.x = data.x;
+            player1.y = data.y;
+            player1.rotation = data.r;
+
+            bullet1.x = data.bx;
+            bullet1.y = data.by;
+            bullet1.rotation = data.br;
+        }
+        
     }
 });
 
@@ -32,24 +59,40 @@ const game = new Phaser.Game(config);
 
 
 //Variables
-let player1;
 let player1_angle = 0;
 
-let player2;
 let player2_angle = 0;
 
-let player_speed = 2;
-let player_rotation_speed = 2;
+let player_speed = 3;
+let player_rotation_speed = 3;
+
+let bullet1;
+let bullet1_speed = 10;
+let bullet1_angle = 0; 
+let bullet1_isShooted = false;
+
+let bullet2;
+let bullet2_speed = 10;
+let bullet2_angle = 0;
+let bullet2_isShooted = false;
+
 
 
 function preload(){
     this.load.image("Player1-img", "assets/PNG/Cars/car_red_small_1.png");
     this.load.image("Player2-img","assets/PNG/Cars/car_blue_small_1.png");
+    this.load.image("Background-img","assets/Background.png");
+    this.load.image("Bullet-img","assets/bullet.png");
 }
 
 function create(){
+    let bg = this.add.image(config.width/2, config.height/2, "Background-img");
+
     player1 = this.add.image(config.width/3, config.height/2,"Player1-img");
     player2 = this.add.image((config.width/3) * 2,config.height/2,"Player2-img");
+
+    bullet1 = this.add.image(config.width/2, config.height/2, "Bullet-img");
+    bullet2 = this.add.image(config.width/2, config.height/2, "Bullet-img");
 
 
     //KEYS
@@ -57,28 +100,93 @@ function create(){
 }
 
 function update(){
+    //bullet stuff
 
-    //Player1 Movement
-    if(cursors.left.isDown){
-        player1_angle -=  player_rotation_speed;
+    bullet1.y -= bullet1_speed*Math.cos(bullet1_angle*Math.PI/180);
+    bullet1.x += bullet1_speed*Math.sin(bullet1_angle*Math.PI/180);
+
+    bullet2.y -= bullet2_speed*Math.cos(bullet2_angle*Math.PI/180);
+    bullet2.x += bullet2_speed*Math.sin(bullet2_angle*Math.PI/180);
+
+    if(player_num == 0){
+        return;
     }
-    else if (cursors.right.isDown){
-        player1_angle +=  player_rotation_speed;
-        
+    else if(player_num == 1){
+        //Player1 Movement
+        if(cursors.left.isDown){
+            player1_angle -=  player_rotation_speed;
+        }
+        else if (cursors.right.isDown){
+            player1_angle +=  player_rotation_speed;
+            
+        }
+
+        if(cursors.up.isDown){
+            player1.y -= player_speed*Math.cos(player1_angle*Math.PI/180);
+            player1.x += player_speed*Math.sin(player1_angle*Math.PI/180);
+        }
+        player1.rotation = player1_angle*Math.PI/180;
+
+        //bullet stuff
+        if(cursors.space.isDown && !bullet1_isShooted){
+            bullet1.x = player1.x;
+            bullet1.y = player1.y;
+            bullet1_angle = player1_angle;
+            bullet1.rotation = player1.rotation;
+            
+            bullet1_isShooted = true;
+        }
+
+        var player_data = {
+            x: player1.x,
+            y: player1.y,
+            r: player1.rotation,
+
+            bx: bullet1.x,
+            by: bullet1.y,
+            br: bullet1.rotation
+        };
+    }
+    else if(player_num == 2){
+        //Player2 Movement
+        if(cursors.left.isDown){
+            player2_angle -=  player_rotation_speed;
+        }
+        else if (cursors.right.isDown){
+            player2_angle +=  player_rotation_speed;
+        }
+
+        if(cursors.up.isDown){
+            player2.y -= player_speed*Math.cos(player2_angle*Math.PI/180);
+            player2.x += player_speed*Math.sin(player2_angle*Math.PI/180);
+        }
+        player2.rotation = player2_angle*Math.PI/180;
+
+        //bullet stuff
+        if(cursors.space.isDown && !bullet2_isShooted){
+            bullet2.x = player2.x;
+            bullet2.y = player2.y;
+            bullet2_angle = player2_angle;
+            bullet2.rotation = player2.rotation;
+            
+            bullet2_isShooted = true;
+        }
+
+        var player_data = {
+            x: player2.x,
+            y: player2.y,
+            r: player2.rotation,
+
+            bx: bullet2.x,
+            by: bullet2.y,
+            br: bullet2.rotation
+        };
     }
 
-    if(cursors.up.isDown){
-        player1.y -= player_speed*Math.cos(player1_angle*Math.PI/180);
-        player1.x += player_speed*Math.sin(player1_angle*Math.PI/180);
-    }
+    
 
-    player1.rotation = player1_angle*Math.PI/180;
+    
 
-    var player_data = {
-        x: player1.x,
-        y: player1.y,
-        r: player1.rotation
-    };
     socket.send(JSON.stringify(player_data));
 
 }
